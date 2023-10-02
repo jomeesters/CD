2096f725e447a16f890dec48fbb7eb840392d032
jomeesters <112581270+jomeesters@users.noreply.github.com>
2023-10-02 13:40:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+# Import what we need from flask
+from flask import Flask
 
+# Create a Flask app inside `app`
+app = Flask(__name__)
+
+# Assign a function to be called when the path `/` is requested
+@app.route('/')
+def index():
+    return 'Hello, world!'
+
+@app.route('/cow')
+def cow():
+    return 'MOoooOo!'
+
+@app.route('/final')
+def final():
+    return 'Welcome to my final assignment!'
+
+@app.route('/pass')
+def passit():
+    return 'I hope I will pass for the first time ^^'
