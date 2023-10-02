92511bda9b04c463f5b41205346a3e1af8ccd96e
jomeesters <112581270+jomeesters@users.noreply.github.com>
2023-10-02 13:21:39 +0200

    Update test_main.py

--- CD/test_main.py
+++ CD/test_main.py
@@ -1,16 +1,7 @@
-import pytest
-
 from main import app
 
-
-@pytest.fixture
-def client():
-    client = app.test_client()
-    return client
-
-
-def test_index(client):
-    response = client.get("/")
-
+def test_index():
+    response = app.test_client().get('/')
+    
+    assert response.data == b'Hello, world...! It works finally again!'
     assert response.status_code == 200
-    assert response.data == b"Hello, welcome to my last assignment for Winc!"
