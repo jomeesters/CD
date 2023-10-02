7c99a164d1f4b424be0fbeb41320dcc9c98b8a59
jomeesters <112581270+jomeesters@users.noreply.github.com>
2023-10-02 13:21:07 +0200

    Update main.py

--- CD/main.py
+++ CD/main.py
@@ -1,8 +1,13 @@
+# Import what we need from flask
 from flask import Flask
 
+# Create a Flask app inside `app`
 app = Flask(__name__)
 
-
-@app.route("/")
+# Assign a function to be called when the path `/` is requested
+@app.route('/')
 def index():
-    return "Hello, welcome to my last assignment for Winc!"
+    return 'Hello, world...! It works finally again!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
