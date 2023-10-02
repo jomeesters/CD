5dae6b3f017fa084f99bc3283c6179c307d99f41
jomeesters <112581270+jomeesters@users.noreply.github.com>
2023-10-02 13:39:31 +0200

    Delete CD directory

--- CD/test_main.py
+++
@@ -1,7 +0,0 @@
-from main import app
-
-def test_index():
-    response = app.test_client().get('/')
-    
-    assert response.data == b'Hello, world...! It works finally again!'
-    assert response.status_code == 200
